6804204bff0b5dfd744b8893d2768d1bd6af7505
Mark Tutkowski <mark@tutkowski.com>
2021-01-02 14:35:42 -0600

    Initial Commit

---
+++ kopps-flavor-forcast-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kopps-flavor-forcast-ios
+//
+//  Created by Mark Tutkowski on 1/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kopps-flavor-forcast-ios/kopps_flavor_forcast_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  kopps_flavor_forcast_iosApp.swift
+//  kopps-flavor-forcast-ios
+//
+//  Created by Mark Tutkowski on 1/2/21.
+//
+
+import SwiftUI
+
+@main
+struct kopps_flavor_forcast_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
